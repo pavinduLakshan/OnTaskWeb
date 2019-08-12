54134512a327ef97b801066f24c4e8603b5f2b1c
pavinduLakshan <174084F@uom.lk>
2019-08-12 10:55:38 +0530

    created subtask item

---
+++ src/components/SubTasks/SubTaskItem.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { Input,ListGroupItem } from 'reactstrap'
+import SENDER from '../../utils/SENDER'
+
+const SubTaskItem = props => {
+
+    function handleChange(e,id){
+        //alert(e.target.checked)
+        SENDER.post('/subtasks/'+id+"/"+e.target.checked).then(
+            res => {
+                props.notifySubtaskStatusChange()
+            }
+        ).catch(err => console.log(err))
+    }    
+
+    return (
+        <ListGroupItem action style={{display: "flex",flexDirection: "row",alignItems: "center",minHeight: "4vh"}}>
+            <Input onChange={e => handleChange(e,props.id)} style={{margin: 0}} type="checkbox" defaultChecked={props.isComplete} id="checkbox2" />
+        <h6 style={{margin: "0% 0% 0% 5%"}}>{props.name}</h6>
+</ListGroupItem>
+    );
+};
+
+export default SubTaskItem;
---
+++ src/components/SubTasks/index.js
@@ -0,0 +1,87 @@
+import React,{ useState, useEffect} from 'react'
+import useForm from '../../utils/useForm'
+import SENDER from '../../utils/SENDER'
+import { Card,CardHeader,CardBody,Form,Input,Button,ListGroup} from 'reactstrap'
+import SubTaskItem from './SubTaskItem'
+import { Checklist } from "styled-icons/octicons/Checklist";
+
+const SubTasks = props => {
+    const [isExpanded,setIsExpanded] = useState(false)
+    const [trig,setTrig] = useState(false)
+    const {values,handleChange,handleSubmit} = useForm(createSubtask)
+    const [subtasks,setSubtasks] = useState([])
+
+    function createSubtask(){
+      console.log({
+        name: values.name,
+        taskId: props.taskId
+      })
+      SENDER.post('/subtasks',{
+        taskId: props.taskId,
+        name: values.name
+      }).then(res => {
+        if(res.status === 200){
+          alert("New Subtask added")
+          setIsExpanded(false)
+          setTrig(!trig)
+        }
+      }).catch(err => console.log(err))
+    }
+
+    function getMarkedSubtaskChange(){
+      setTrig(!trig)
+    }
+
+    useEffect(
+      () => {
+        SENDER.get('/subtasks/task/'+props.taskId).then(
+          res => {
+            let i=0; 
+            console.log("subtasks: ")
+            console.log(res.data)
+            setSubtasks(res.data)
+            i = res.data.filter( subtask => subtask.completed === true ).length
+            props.sendSubTaskStats(i,res.data.length)
+          }
+        ).catch(err => console.log(err))
+      },[trig]
+    )
+    return (
+        <Card>
+                <CardHeader>
+                  <Checklist size={20} />
+                  <b>Subtasks</b>
+                  <div className="card-header-actions">
+                    <i
+                      style={{
+                        cursor: "pointer",
+                        display: props.isAdmin ? "block" : "none",
+                      }}
+                      className={isExpanded ? "fa fa-minus float-right" : "fa fa-plus float-right"}
+                      onClick={() => setIsExpanded(!isExpanded)}
+                    />
+                  </div>
+                </CardHeader>
+                <CardBody style={{padding: 0}}>
+                <Form onSubmit={handleSubmit} inline style={{margin: "1%",display: isExpanded ? "block" : "none"}}>
+                      <Input style={{width: "83%"}} name="name" onChange={handleChange} placeholder="Add subtask"></Input>
+                      <Button color="success" style={{float: "right"}}>Add</Button>
+                  </Form>
+                  <ListGroup>
+                    {subtasks.map( subtask => {
+                      return <SubTaskItem 
+                        taskId={props.taskId}
+                        key={subtask.id}
+                        id={subtask.id}
+                        isComplete={subtask.completed}
+                        notifySubtaskStatusChange={getMarkedSubtaskChange}
+                        name={subtask.name}
+                      />
+                    })}
+                  </ListGroup>
+                </CardBody>
+              </Card>
+    )
+}
+
+export default SubTasks
