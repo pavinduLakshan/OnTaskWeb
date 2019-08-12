4291b311fc3f213d3ec920399a41fac21a750282
pavinduLakshan <174084F@uom.lk>
2019-08-12 10:57:52 +0530

    created adding members at group init function

---
+++ src/components/MemberSearchItem/index.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Button, ListGroupItem } from "reactstrap";
+
+class MemberSearchItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        selected: false
+    }
+  }
+
+  render() {
+    return (
+      <div
+      style={{
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "row",
+        paddingBottom: "2%"
+      }}
+      >
+        <ListGroupItem
+          action
+          tag="a"
+          href={"/users/" + this.props.id}
+          style={{
+            padding: "0.5%",
+            border: 0,
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "row",
+          }}
+          className="list-group-item-accent-warning"
+        >
+          <img
+            style={{ borderRadius: "50%" }}
+            className="img-avatar"
+            width="25"
+            height="25"
+            src={this.props.data.propicURL}
+            alt=""
+          />
+          <div style={{ marginLeft: "1%" }}>{this.props.name} </div>
+        </ListGroupItem>
+        <div style={{ flexGrow: 1 }} />
+         {this.props.selected ? "" : <Button
+            color="success"
+            onClick={() => this.props.selectMember(this.props.data)}
+            style={{marginLeft: "1%"}}
+          >
+            Add
+          </Button>}  
+      </div>
+    );
+  }
+}
+
+export default MemberSearchItem;
--- src/components/NewGroupForm/GroupForm.js
+++ src/components/NewGroupForm/GroupForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useCallBack } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
@@ -6,13 +6,57 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import useForm from "../../utils/useForm";
 import { withRouter } from "react-router-dom";
-// import { SearchAlt2 } from "styled-icons/boxicons-regular/SearchAlt2";
+import { SearchAlt2 } from "styled-icons/boxicons-regular/SearchAlt2";
 import InputGroup from "react-bootstrap/InputGroup"
 import SENDER from "../../utils/SENDER";
+import MemberSearchItem from "../MemberSearchItem";
 
 const GroupForm = props => {
   const { values, handleChange, handleSubmit } = useForm(createNewGroup);
+  const [groupMembers,setGroupMembers] = useState([])
+  const [trig,setTrig] = useState(true)
+  const [searchResults,setSearchResults] = useState([])
   const [NOfChars, setNOfChars] = useState(160);
+
+  const handleMemberSearch = e => {
+    if(e.target.value){
+      SENDER.get('/user/search/'+e.target.value).then(
+        res => {
+          let result = res.data
+
+          for (var i = 0, len = groupMembers.length; i < len; i++) { 
+            for (var j = 0, len2 = result.length; j < len2; j++) { 
+                if (groupMembers[i].userId === result[j].userId) {
+                    result.splice(j, 1);
+                    len2=result.length;
+                }
+            }
+        }
+
+        console.log(result)  
+        setSearchResults(result)
+        }
+      ).catch(err => console.log(err))
+    }
+    else{
+      setSearchResults([])
+    }
+  }
+
+  function addMember(data){
+      console.log("addd")
+      console.log(data)
+      const newSR = searchResults.filter(function(value, index, arr){
+
+        return value !== data;
+    
+    });
+    setSearchResults(newSR)
+    groupMembers.push(data)
+      setTrig(!trig)
+      console.log([...groupMembers])
+    }
+
 
   function createNewGroup() {
     console.log(values);
@@ -20,10 +64,12 @@ const GroupForm = props => {
       userId: localStorage.getItem("id"),
       name: values.name,
       description: values.description,
+      members: groupMembers.map( member => member.userId)
     })
       .then(res => {
         if (res.status === 200) {
           alert("New group was created successfully");
+          window.location.reload()
         }
       })
       .catch(err => {
@@ -70,16 +116,38 @@ const GroupForm = props => {
           <InputGroup>
             <Form.Control
               type="text"
+              onChange={handleMemberSearch}
               placeholder="Search"
               aria-describedby="inputGroupPrepend"
-              required
             />
             <InputGroup.Append style={{cursor: "pointer"}}>
               <InputGroup.Text id="inputGroupPrepend">
-              {/* <SearchAlt2 size={20} /> */}
+               <SearchAlt2 size={20} /> 
               </InputGroup.Text>
             </InputGroup.Append>
           </InputGroup>
+          <div className="selected" style={{paddingTop: "2%"}}>
+          {groupMembers ? groupMembers.map( result => {
+              const lname = result.lname ? result.lname : ""
+              return <MemberSearchItem
+                key={result.userId}
+                data={result}
+                selected={true}
+                name={result.fname+""+lname}
+              ></MemberSearchItem>
+            }): <></>}
+          </div>
+          <div className="search_results" style={{paddingTop: "2%"}}> 
+            {searchResults.map( result => {
+              const lname = result.lname ? result.lname : ""
+              return <MemberSearchItem
+                key={result.userId}
+                data={result}
+                selectMember={addMember}
+                name={result.fname+""+lname}
+              ></MemberSearchItem>
+            })}
+          </div>
         </Col>
       </Row>
       <Modal.Footer>
