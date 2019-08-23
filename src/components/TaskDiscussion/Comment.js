da4607253090ce9020edb5891412cc39ac6198b1
pavinduLakshan <174084F@uom.lk>
2019-08-23 18:40:19 +0530

    added emojis & translated UTC to local time

--- src/components/TaskDiscussion/Comment.js
+++ src/components/TaskDiscussion/Comment.js
@@ -3,10 +3,18 @@ import Card from 'react-bootstrap/Card'
 import ReactMarkdown from 'react-markdown'
 
 const Comment = props => {
+    const CreatedDateFields = new Date(props.createdAt).toString().split(' ')
+    //alert(CreatedDateFields.slice(1,4))
+
+    const createdDate = CreatedDateFields.slice(1,4).toString().replace(/,/,"")
+    const createdTime = CreatedDateFields[4]
+
     return (
         <Card style={{marginTop: "1%",marginBottom: "1%",height: "15vh"}}>
             <Card.Header>
-                <p style={{margin: 0}}><b>{props.fname}</b> commented on {props.createdAt.slice(0,10)} at {props.createdAt.slice(12,19)}</p>
+                <p style={{margin: 0}}><b>{props.fname}</b> commented on {createdDate} at {createdTime}
+                </p>
+                {/* commented on {props.createdAt.slice(0,10)} at {props.createdAt.slice(12,19)} */}
             </Card.Header>
               <Card.Body style={{padding: "1% 0% 1% 4%"}}>
               <ReactMarkdown source={props.content} />
--- src/components/TaskDiscussion/CommentBox.js
+++ src/components/TaskDiscussion/CommentBox.js
@@ -1,53 +1,108 @@
-import React,{ useState } from 'react'
-import Tabs from 'react-bootstrap/Tabs'
-import SENDER from '../../utils/SENDER'
-import { Input,Button} from 'reactstrap'
-import Tab from 'react-bootstrap/Tab'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import Tabs from "react-bootstrap/Tabs";
+import SENDER from "../../utils/SENDER";
+import { Input, Button } from "reactstrap";
+import Tab from "react-bootstrap/Tab";
+import ReactMarkdown from "react-markdown";
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
 
-const CommentBox = props => {
-    const [input,setInput] = useState("")
-
-    function handleChange(e){
-        setInput(e.target.value)
-    } 
-
-    function postComment(event){
-        setInput("pppp")
-        event.preventDefault()
-        // console.log({
-        //     taskId: props.taskId,
-        //     content: input,
-        //     userId: parseInt(localStorage.getItem('id'))
-        // })
-        SENDER.post('/comments',{
-            taskId: props.taskId,
-            content: input,
-            userId: parseInt(localStorage.getItem('id'))
-        }).then(
-            res => {
-                if(res.status === 200){
-                    
-                    //alert("New comment Added")
-                }
-                //props.onAdd()
-            }
-        ).catch(err => console.log("Comment Error : "+err))
-    }
-
-    return(
-        <>
-        <Tabs defaultActiveKey="write" id="uncontrolled-tab-example">
-  <Tab eventKey="write" title="Write" style={{padding: 0}}>
-    <Input type="textarea" name="input" onChange={handleChange} id="exampleText" />
-  </Tab>
-  <Tab eventKey="preview" title="Preview">
-    <ReactMarkdown source={input} />
-  </Tab>
-</Tabs>
-<Button color="success" onClick={postComment} style={{marginTop: "1%",float: "right"}}>Comment</Button>
-</>
-    )
+const emojiDeselectedStyle = {
+  color: "gray", 
+  //textShadow: "0 0 0 red"
 }
 
-export default CommentBox
+const emojiSelectedStyle = {
+  cursor: "pointer",marginLeft: "auto",paddingTop: "2%"
+}
+
+
+
+const CommentBox = props => {
+  const [input, setInput] = useState("");
+  const [isShown, setIsShown] = useState(false);
+
+  function controlEmojiPickerShow() {
+    setIsShown(!isShown);
+  }
+
+  function handleChange(e) {
+    setInput(e.target.value);
+  }
+
+  function postComment(event) {
+    setInput("");
+    event.preventDefault();
+    // console.log({
+    //     taskId: props.taskId,
+    //     content: input,
+    //     userId: parseInt(localStorage.getItem('id'))
+    // })
+    SENDER.post("/comments", {
+      taskId: props.taskId,
+      content: input,
+      userId: parseInt(localStorage.getItem("id")),
+    })
+      .then(res => {
+        if (res.status === 200) {
+          //alert("New comment Added")
+        }
+        //props.onAdd()
+      })
+      .catch(err => console.log("Comment Error : " + err));
+  }
+
+  return (
+    <>
+      <Tabs defaultActiveKey="write" id="uncontrolled-tab-example">
+        <Tab eventKey="write" title="Write" style={{ padding: 0 }}>
+          <Input
+            type="textarea"
+            name="input"
+            value={input}
+            onChange={handleChange}
+          />
+        </Tab>
+        <Tab eventKey="preview" title="Preview">
+          <ReactMarkdown source={input} />
+        </Tab>
+      </Tabs>
+      <div
+        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+      >
+        <p style={ isShown ? emojiSelectedStyle : emojiDeselectedStyle} onClick={controlEmojiPickerShow}>
+          🙂
+        </p>
+        <div style={{ flexGrow: 1 }} />
+        <Button
+          color="success"
+          onClick={postComment}
+          style={{ marginTop: "1%", float: "right" }}
+        >
+          Comment
+        </Button>
+      </div>
+
+      <Picker
+        style={{
+          display: isShown ? "block" : "none",
+          height: "40vh",
+          marginTop: "1%",
+          overflowX: "hidden",
+          zIndex: 10
+        }}
+        emoji=""
+        showSkinTones={false}
+        showPreview={false}
+        title=""
+        set="facebook"
+        onSelect={emoji => {
+          console.log(emoji);
+          setInput(input+emoji.native)
+        }}
+      />
+    </>
+  );
+};
+
+export default CommentBox;
