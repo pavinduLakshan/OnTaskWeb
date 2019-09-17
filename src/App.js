3d3b2fe98d75a799e8f73e326dd5bce0f813447f
pavinduLakshan <174084F@uom.lk>
2019-09-17 18:50:25 +0530

    removed unnecessary comments & code

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const DefaultLayout = React.lazy(() => import('./containers/DefaultLayout'));
 
 const Login = React.lazy(() => import('./views/Login/Login'));
 const Signup = React.lazy(() => import('./views/Signup'));
+const ForgotPassword = React.lazy(() => import('./views/ForgotPassword'));
 const Page404 = React.lazy(() => import('./views/Page404'));
 const Page500 = React.lazy(() => import('./views/Page500'));
 const Group = React.lazy(() => import('./views/Group'));
@@ -25,7 +26,7 @@ class App extends Component {
               <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
               <Route exact path="/signup" name="Signup Page" render={props => <Signup {...props}/>} />
               <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              {/* <Route exact path="/groups/:gid" name="Group Page" render={props => <Group {...props}/>} /> */}
+              <Route exact path="/forgot-password" name="Forgot Password" render={props => <ForgotPassword {...props}/>}/>
               <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} /> 
             </Switch>
           </React.Suspense>
