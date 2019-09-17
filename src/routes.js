463cafdeee155f784621126a9497d8c9a31267c3
pavinduLakshan <174084F@uom.lk>
2019-09-17 19:45:37 +0530

    removed unwanted components

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,11 @@ import { withRouter} from 'react-router-dom'
 const Dashboard = React.lazy(() => import('./views/Dashboard'));
 const Group = React.lazy(() => import('./views/Group'));
 const Profile = React.lazy(() => import('./views/Profile'));
-const Users = React.lazy(() => import('./views/Users/Users'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/groups/:gid', exact: true,name: 'Group', component: withRouter(Group) },
-  { path: '/profile', name: 'Profile', component: Profile },
-  { path: '/users', exact: true,  name: 'Users', component: Users },
   { path: '/users/:id', exact: true, name: 'User Details', component: Profile },
 ];
 
